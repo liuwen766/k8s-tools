0a55570bd544c3011a5f16ece48de2e62c14515e
liuwen2 <liuwen2@cmss.chinamobile.com>
2023-08-02 16:39:48 +0800

    k8s-声明式API原理

---
+++ 深入剖析 Kubernetes/05-容器编排与Kubernetes作业管理 (15讲)/24丨深入解析声明式API（一）：API对象的奥秘/k8s-controller-custom-resource/pkg/apis/samplecrd/register.go
@@ -0,0 +1,7 @@
+package samplecrd
+
+// 用来放置后面要用到的全局变量。
+const (
+	GroupName = "samplecrd.k8s.io"
+	Version   = "v1"
+)
---
+++ 深入剖析 Kubernetes/05-容器编排与Kubernetes作业管理 (15讲)/24丨深入解析声明式API（一）：API对象的奥秘/k8s-controller-custom-resource/pkg/apis/samplecrd/v1/doc.go
@@ -0,0 +1,6 @@
+// +k8s:deepcopy-gen=package
+
+// +groupName=samplecrd.k8s.io
+package v1
+
+//+<tag_name>[=value] 格式的注释 就是 Kubernetes 进行代码生成要用的 Annotation 风格的注释。
