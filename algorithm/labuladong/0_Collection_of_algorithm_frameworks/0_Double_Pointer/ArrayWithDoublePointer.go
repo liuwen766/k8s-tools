304de115bb20cbb9b760273922be9b8e663d9d3a
liuwen766 <780442619@qq.com>
2024-01-09 23:00:06 +0800

    双指针秒杀七道数组题

--- algorithm/labuladong/0_Collection_of_algorithm_frameworks/0_Double_Pointer/ArrayWithDoublePointer.go
+++ algorithm/labuladong/0_Collection_of_algorithm_frameworks/0_Double_Pointer/ArrayWithDoublePointer.go
@@ -1,1 +1,45 @@
 package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 3, 5, 9, 9, 10, 15, 15, 18}
+	sum := twoSum(arr, 9)
+	fmt.Println("arr:", arr, "sum:", sum)
+}
+
+// 1、两数之和
+func twoSum(numbers []int, target int) []int {
+	return nil
+}
+
+// 2、删除有序数组中的重复项
+func removeDuplicates(nums []int) int {
+	return -1
+}
+
+// 3、移除元素
+func removeElement(nums []int, val int) int {
+	return -1
+}
+
+// 4、移动零
+func moveZeroes(nums []int) {
+
+}
+
+// 5、反转字符串
+// 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+func reverseString(s []byte) {
+
+}
+
+// 6、最长回文子串
+func longestPalindrome(s string) string {
+	return ""
+}
+
+// 7、删除排序链表中的重复元素
+func deleteDuplicates(head *ListNode) *ListNode {
+	return nil
+}
