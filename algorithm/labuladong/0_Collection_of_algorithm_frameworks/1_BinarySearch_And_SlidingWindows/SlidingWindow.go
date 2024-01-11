7dac64559fb14b6eab6e1bef19214d6769484b9f
liuwen2 <liuwen2@cmss.chinamobile.com>
2024-01-11 18:05:53 +0800

    滑动窗口算法完结

--- algorithm/labuladong/0_Collection_of_algorithm_frameworks/1_BinarySearch_And_SlidingWindows/SlidingWindow.go
+++ algorithm/labuladong/0_Collection_of_algorithm_frameworks/1_BinarySearch_And_SlidingWindows/SlidingWindow.go
@@ -27,6 +27,7 @@ func lengthOfLongestSubstring(s string) int {
 		right++
 		windows[c]++
 
+		// TODO ☆☆☆☆☆☆
 		for windows[c] > 1 {
 			d := s[left]
 			left++
@@ -43,14 +44,84 @@ func lengthOfLongestSubstring(s string) int {
 // 2、找到字符串中所有字母异位词
 // 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
 func findAnagrams(s string, p string) []int {
-	// todo
-	return nil
+	windows := make(map[byte]int)
+	need := make(map[byte]int)
+	for i := range p {
+		need[p[i]]++
+	}
+
+	left := 0
+	right := 0
+
+	valid := 0
+	res := make([]int, 0)
+	for right < len(s) {
+		c := s[right]
+		right++
+		if _, ok := need[c]; ok {
+			windows[c]++
+			if windows[c] == need[c] {
+				valid++
+			}
+		}
+		// TODO ☆☆☆☆☆☆
+		for right-left >= len(p) {
+			if valid == len(need) {
+				res = append(res, left)
+			}
+
+			d := s[left]
+			left++
+			if _, ok := need[d]; ok {
+				if windows[d] == need[d] {
+					valid--
+				}
+				windows[d]--
+			}
+		}
+	}
+
+	return res
 }
 
 // 3、字符串的排列
 // 给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。如果是，返回 true ；否则，返回 false 。
 func checkInclusion(s1 string, s2 string) bool {
-	// todo
+	windows := make(map[byte]int)
+	need := make(map[byte]int)
+	for i := range s1 {
+		need[s1[i]]++
+	}
+
+	left := 0
+	right := 0
+	valid := 0
+
+	for right < len(s2) {
+		c := s2[right]
+		right++
+		if _, ok := need[c]; ok {
+			windows[c]++
+			if windows[c] == need[c] {
+				valid++
+			}
+		}
+
+		// TODO ☆☆☆☆☆☆
+		for right-left >= len(s1) {
+			if valid == len(need) {
+				return true
+			}
+			d := s2[left]
+			left++
+			if _, ok := need[d]; ok {
+				if windows[d] == need[d] {
+					valid--
+				}
+				windows[d]--
+			}
+		}
+	}
 	return false
 }
 
@@ -82,6 +153,7 @@ func minWindow(s string, t string) string {
 			}
 		}
 
+		// TODO ☆☆☆☆☆☆
 		for valid == len(need) {
 			if right-left < length {
 				startIndex = left
