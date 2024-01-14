33af09e80af94ad270a52d24b057ef4663bec2f5
liuwen766 <780442619@qq.com>
2024-01-14 19:33:23 +0800

    动态规划——斐波那契数的三种解法

--- algorithm/labuladong/0_Collection_of_algorithm_frameworks/3_Dynamic_Programming/Dynamic.go
+++ algorithm/labuladong/0_Collection_of_algorithm_frameworks/3_Dynamic_Programming/Dynamic.go
@@ -6,12 +6,41 @@ func main() {
 	fmt.Println("动态规划解题思路：明确 base case -> 明确「状态」-> 明确「选择」 -> 定义 dp 数组/函数的含义")
 }
 
-// 1、斐波那契数——暴力递归
+// 1、斐波那契数解法1——暴力递归
 func fib1(n int) int {
-	return 0
+	if n == 0 || n == 1 {
+		return n
+	}
+	res := fib1(n-1) + fib1(n-2)
+	return res
 }
 
-// 1、斐波那契数——带备忘录的递归解法
+// 1、斐波那契数解法2——带备忘录的递归解法
 func fib2(n int) int {
-	return 0
+	bak := make([]int, n+1)
+	return bib2withBak(bak, n)
+}
+
+func bib2withBak(bak []int, n int) int {
+	if n == 0 || n == 1 {
+		return n
+	}
+	if bak[n] != 0 {
+		return bak[n]
+	}
+	return bib2withBak(bak, n-1) + bib2withBak(bak, n-2)
+}
+
+// 1、斐波那契数解法3——带备忘录的迭代解法
+func fib3(n int) int {
+	if n == 0 || n == 1 {
+		return n
+	}
+	dp := make([]int, n+1)
+	dp[0] = 0
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
 }
