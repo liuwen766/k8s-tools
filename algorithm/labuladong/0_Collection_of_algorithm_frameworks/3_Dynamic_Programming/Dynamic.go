5c147af424fb3154451e8f0b7c0369a37aae7538
liuwen766 <780442619@qq.com>
2024-01-14 10:26:42 +0800

    反转链表

---
+++ algorithm/labuladong/0_Collection_of_algorithm_frameworks/3_Dynamic_Programming/Dynamic.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("动态规划解题思路：明确 base case -> 明确「状态」-> 明确「选择」 -> 定义 dp 数组/函数的含义")
+}
--- algorithm/labuladong/1_DataStructure/0_ListNode/reverseListNode.go
+++ algorithm/labuladong/1_DataStructure/0_ListNode/reverseListNode.go
@@ -14,7 +14,16 @@ type ListNode struct {
 
 // 1、反转链表1
 func reverseList1(head *ListNode) *ListNode {
-	var pre *ListNode
+	var pre, cur, nxt *ListNode
+	pre = nil
+	cur = head
+	nxt = head
+	for cur != nil {
+		nxt = cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = nxt
+	}
 	return pre
 }
 
@@ -30,16 +39,63 @@ func reverseList2(head *ListNode) *ListNode {
 }
 
 // 2、反转链表——反转链表的前n个节点
+// 1 2 3 4 5
+// 3 2 1 4 5
 func reverseN(head *ListNode, n int) *ListNode {
-	return head
+	var successor *ListNode
+	if n == 1 {
+		successor = head.Next
+		return head
+	}
+
+	newHead := reverseN(head.Next, n-1)
+	head.Next.Next = head
+	head.Next = successor
+	return newHead
 }
 
 // 3、反转链表——反转从位置 left 到位置 right 的链表节点
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	// base case
+	if left == 1 {
+		return reverseN(head, right)
+	}
+	// 前进到反转的起点触发 base case
+	head.Next = reverseBetween(head.Next, left-1, right-1)
 	return head
 }
 
 // 4、反转链表——K个一组翻转链表
 func reverseKGroup(head *ListNode, k int) *ListNode {
-	return head
+	if head == nil || head.Next == nil {
+		return head
+	}
+	a := head
+	b := head
+	for i := 0; i < k; i++ {
+		// ☆☆  说明不用反转了，直接返回头结点即可
+		if b == nil {
+			return head
+		}
+		b = b.Next
+	}
+
+	// 先反转前k个，即a到b个
+	newHead := reverseAandB(a, b)
+	// 再递归
+	a.Next = reverseKGroup(b, k)
+	return newHead
+}
+
+func reverseAandB(a *ListNode, b *ListNode) *ListNode {
+	var pre, cur, nxt *ListNode
+	cur = a
+	nxt = a
+	for cur != b {
+		nxt = cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = nxt
+	}
+	return pre
 }
