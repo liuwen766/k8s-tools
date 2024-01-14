49e6ec58ef515311f42a01fa9ae40e85a9e6f2c2
liuwen766 <780442619@qq.com>
2024-01-14 17:01:10 +0800

    动态规划——初始化

--- algorithm/labuladong/0_Collection_of_algorithm_frameworks/3_Dynamic_Programming/Dynamic.go
+++ algorithm/labuladong/0_Collection_of_algorithm_frameworks/3_Dynamic_Programming/Dynamic.go
@@ -5,3 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("动态规划解题思路：明确 base case -> 明确「状态」-> 明确「选择」 -> 定义 dp 数组/函数的含义")
 }
+
+// 1、斐波那契数——暴力递归
+func fib1(n int) int {
+	return 0
+}
+
+// 1、斐波那契数——带备忘录的递归解法
+func fib2(n int) int {
+	return 0
+}
